b0db32e4249273aee0d561987d2f0e6e389982d3
Veljko Radenkovic <veljko.radenkovic@gmail.com>
2023-11-28 13:38:14 +0100

    added debug trace

--- src/KafkaConnector.php
+++ src/KafkaConnector.php
@@ -14,21 +14,26 @@ class KafkaConnector implements ConnectorInterface
      */
     public function connect(array $config)
     {
-        $conf = new \RdKafka\Conf();
-        $conf->set('bootstrap.servers', $config['bootstrap_servers']);
-        $conf->set('security.protocol', $config['security_protocol']);
-        $conf->set('sasl.mechanism', $config['sasl_mechanisms']);
-        $conf->set('sasl.username', $config['sasl_username']);
-        $conf->set('sasl.password', $config['sasl_password']);
+        try {
+            $conf = new \RdKafka\Conf();
+            $conf->set('bootstrap.servers', $config['bootstrap_servers']);
+            $conf->set('security.protocol', $config['security_protocol']);
+            $conf->set('sasl.mechanism', $config['sasl_mechanisms']);
+            $conf->set('sasl.username', $config['sasl_username']);
+            $conf->set('sasl.password', $config['sasl_password']);
 
-        $producer = new \RdKafka\Producer($conf);
+            $producer = new \RdKafka\Producer($conf);
 
-        $conf->set('group.id', $config['group_id']);
-        $conf->set('auto.offset.reset', 'earliest');
-        $conf->set('enable.partition.eof', 'true');
+            $conf->set('group.id', $config['group_id']);
+            $conf->set('auto.offset.reset', 'earliest');
+            $conf->set('enable.partition.eof', 'true');
 
-        $consumer = new \RdKafka\Consumer($conf);
+            $consumer = new \RdKafka\Consumer($conf);
 
-        return new KafkaQueue($consumer, $producer);
+            return new KafkaQueue($consumer, $producer);
+        } catch (\Exception $exception) {
+            var_dump($exception);
+        }
+
     }
 }
